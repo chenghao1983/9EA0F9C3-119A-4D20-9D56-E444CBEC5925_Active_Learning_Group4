21c9b8cb9590b8c6fa80b3fb0a0663d3bfe701c4
chenghao1983 <joechenghao@gmail.com>
2016-04-11 23:25:10 +0800

    1. add structure of DB scripts 2. add empty data dic doc

---
+++ DB/SQL/7_Others/Patch_DBVersion.sql
@@ -0,0 +1,4 @@
+
+DECLARE @DBVersion varchar(255)
+SET @DBVersion='Schema revision 1.0'
+updated DBVersion set DBVersion = @DBVersion
---
+++ DB/SQL/GenerateCleanDB.bat
@@ -0,0 +1,16 @@
+﻿cls
+@ECHO --------------------------Structure----------------- >  ENET_Project_Active_Learning_Group4_CleanDB.sql
+type 1_Setup\Struct.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------Index--------------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+type 2_Index\Index.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------Function------------------ >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+type 3_Function\Function.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------View---------------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+type 4_View\View.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------SP------------------------ >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+type 5_SP\SP.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------Data---------------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+type 6_Data\Data.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------Others---------------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+type 7_Others\Patch_DBVersion.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+pause>nul
