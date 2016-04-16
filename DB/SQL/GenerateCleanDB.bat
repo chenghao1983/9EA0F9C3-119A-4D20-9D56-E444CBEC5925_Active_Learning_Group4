ceb42bcd17b02a1811f04bf1b29a78c0c884aa1b
chenghao1983 <joechenghao@gmail.com>
2016-04-16 11:12:36 +0800

    1. updated DB instance name

--- DB/SQL/GenerateCleanDB.bat
+++ DB/SQL/GenerateCleanDB.bat
@@ -1,19 +1,19 @@
 cls
-break> ENET_Project_Active_Learning_Group4_CleanDB.sql 
-@ECHO --------------------------Structure----------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql 
-type 1_Setup\Struct.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-@ECHO --------------------------Index--------------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-type 2_Index\Index.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-@ECHO --------------------------Function------------------ >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-type 3_Function\Function.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-@ECHO --------------------------View---------------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-type 4_View\View.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-@ECHO --------------------------SP------------------------ >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-type 5_SP\SP.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-@ECHO --------------------------Data---------------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-type 6_Data\Data.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-@ECHO --------------------------Others---------------------- >> ENET_Project_Active_Learning_Group4_CleanDB.sql
-type 7_Others\Patch_DBVersion.sql >> ENET_Project_Active_Learning_Group4_CleanDB.sql
+break> Active_Learning_Group4_CleanDB.sql 
+@ECHO --------------------------Structure----------------- >> Active_Learning_Group4_CleanDB.sql 
+type 1_Setup\Struct.sql >> Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------Index--------------------- >> Active_Learning_Group4_CleanDB.sql
+type 2_Index\Index.sql >> Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------Function------------------ >> Active_Learning_Group4_CleanDB.sql
+type 3_Function\Function.sql >> Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------View---------------------- >> Active_Learning_Group4_CleanDB.sql
+type 4_View\View.sql >> Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------SP------------------------ >> Active_Learning_Group4_CleanDB.sql
+type 5_SP\SP.sql >> Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------Data---------------------- >> Active_Learning_Group4_CleanDB.sql
+type 6_Data\Data.sql >> Active_Learning_Group4_CleanDB.sql
+@ECHO --------------------------Others---------------------- >> Active_Learning_Group4_CleanDB.sql
+type 7_Others\Patch_DBVersion.sql >> Active_Learning_Group4_CleanDB.sql
 
 @ECHO DONE !
 
