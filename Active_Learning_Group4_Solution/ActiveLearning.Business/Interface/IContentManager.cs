c8d7882c6ce5d3128dbb7f3cfabefc42dd7686f2
chenghao1983 <joechenghao@gmail.com>
2016-04-17 00:48:18 +0800

    updated

---
+++ Active_Learning_Group4_Solution/ActiveLearning.Business/Interface/IContentManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActiveLearning.DB;
+
+namespace ActiveLearning.Business.Interface
+{
+    public interface IContentManager
+    {
+        IEnumerable<Content> GetContentByCourseSid(int courseSid);
+        Content GetActiveContentBySid(int sid);
+        Content AddContent(Content content, int courseSid);
+        bool RemoveContent(Content content);
+        bool UpdateContent(Content content);
+
+
+    }
+}
