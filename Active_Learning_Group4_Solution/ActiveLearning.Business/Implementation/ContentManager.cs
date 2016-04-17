f5a32a01304950049f7920d2b69dba11c83b066c
chenghao1983 <joechenghao@gmail.com>
2016-04-17 09:15:48 +0800

    updated

---
+++ Active_Learning_Group4_Solution/ActiveLearning.Business/Implementation/ContentManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActiveLearning.DB;
+using ActiveLearning.Business.Interface;
+using ActiveLearning.Business.Common;
+
+namespace ActiveLearning.Business.Implementation
+{
+    public class ContentManager : BaseManager, IContentManager
+    {
+        public Content AddContent(Content content, int CourseSid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Content GetActiveContentBySid(int Sid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Content> GetContentByCourseSid(int CourseSid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveContent(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateContent(Content content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
