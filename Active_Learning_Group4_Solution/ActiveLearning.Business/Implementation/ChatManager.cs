6ad7f48d0c9c9946403fcec88331ee030924a7f3
chenghao1983 <joechenghao@gmail.com>
2016-04-17 20:39:06 +0800

    1. update DB to 1.3

--- Active_Learning_Group4_Solution/ActiveLearning.Business/Implementation/ChatManager.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.Business/Implementation/ChatManager.cs
@@ -5,11 +5,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ActiveLearning.DB;
-
+using ActiveLearning.Business.Common;
 namespace ActiveLearning.Business.Implementation
 {
     public class ChatManager : BaseManager, IChatManager
     {
-    
+        public Chat AddChat(Chat chat, out string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteChat(Chat chat, out string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Chat> GetAllChat(out string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chat GetChatByChatSid(int chatSid, out string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetMessageByGroup(string groupName, out string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendMessageToGroup(string groupName, string messge, out string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateChat(Chat chat, out string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Active_Learning_Group4_Solution/ActiveLearning.Business/Interface/IChatManager.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.Business/Interface/IChatManager.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ActiveLearning.DB;
 namespace ActiveLearning.Business.Interface
 {
     public interface IChatManager : IDisposable
     {
+        Chat GetChatByChatSid(int chatSid, out string message);
+        //ChatRecord GetChatRecordByChatRecordSid(int chatRecordSid, out string message);
+        IEnumerator<Chat> GetAllChat(out string message);
+        Chat AddChat(Chat chat, out string message);
+        bool UpdateChat(Chat chat, out string message);
+        bool DeleteChat(Chat chat, out string message);
+        bool SendMessageToGroup(string groupName, string messge, out string message);
+        bool GetMessageByGroup(string groupName, out string message);
 
     }
 }
