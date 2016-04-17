83dc37d201392525649eb3f7d0c87bbe09ae5576
chenghao1983 <joechenghao@gmail.com>
2016-04-17 17:02:18 +0800

    1. updated user manager

--- Active_Learning_Group4_Solution/ActiveLearning.Business/Interface/IUserManager.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.Business/Interface/IUserManager.cs
@@ -10,24 +10,24 @@ namespace ActiveLearning.Business.Interface
     public interface IUserManager
     {
         bool UserNameExists(string userName, out string message);
-        Student GetStudentByStudentSid(int sid, out string message);
-        Student GetActiveStudentByStudentSid(int sid, out string message);
+        Student GetStudentByStudentSid(int studentSid, out string message);
+        Student GetActiveStudentByStudentSid(int studentSid, out string message);
         IEnumerator<Student> GetAllActiveStudent(out string message);
         Student AddStudent(Student student, out string message);
         bool UpdateStudent(Student student, out string message);
         bool DeleteStudent(Student student, out string message);
-        Instructor GetInstructorByInstructorSid(int sid, out string message);
-        Instructor GetActiveInstructorByInstructorSid(int sid, out string message);
+        Instructor GetInstructorByInstructorSid(int instructorSid, out string message);
+        Instructor GetActiveInstructorByInstructorSid(int instructorSid, out string message);
         IEnumerator<Instructor> GetAllActiveInstructor( out string message);
         Instructor AddInstructor(Instructor instructor, out string message);
         bool UpdateInstructor(Instructor instructor, out string message);
         bool DeleteInstructor(Instructor instructor, out string message);
-        Admin GetAdminByAdminSid(int sid, out string message);
-        Admin GetActiveAdminByAdminSid(int sid, out string message);
+        Admin GetAdminByAdminSid(int adminSid, out string message);
+        Admin GetActiveAdminByAdminSid(int adminSid, out string message);
         IEnumerator<Admin> GetAllActiveAdmin(out string message);
         Admin AddAdmin(Admin admin, out string message);
         bool UpdateAdmin(Admin admin, out string message);
         bool DeleteAdmin(Admin admin, out string message);
-        User IsAuthenticated(string userID, string pass, out string messge);
+        User IsAuthenticated(string userName, string password, out string messge);
     }
 }
