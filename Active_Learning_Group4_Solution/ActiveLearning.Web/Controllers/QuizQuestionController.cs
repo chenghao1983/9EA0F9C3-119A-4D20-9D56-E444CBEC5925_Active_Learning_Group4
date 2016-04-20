23de96374d7629add3e1b9044c02bb60407da523
sheng kiat <sheng kiat@DESKTOP-ESER2VA>
2016-04-20 12:06:31 +0800

    Update

---
+++ Active_Learning_Group4_Solution/ActiveLearning.Web/Controllers/QuizQuestionController.cs
@@ -0,0 +1,34 @@
+﻿using ActiveLearning.Business.Implementation;
+using ActiveLearning.DB;
+using ActiveLearning.Repository.Context;
+using ActiveLearning.Web.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ActiveLearning.Web.Controllers
+{
+    public class QuizQuestionController : ApiController
+    {
+        private ActiveLearningContext db;
+
+        private QuizManager quizManager = new QuizManager();
+        private StatisticsService statisticsService;
+
+        public QuizQuestionController()
+        {
+            this.db = new ActiveLearningContext();
+            this.statisticsService = new StatisticsService();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Active_Learning_Group4_Solution/ActiveLearning.Web/Hubs/StatisticsHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+namespace ActiveLearning.Web.Hubs
+{
+    public class StatisticsHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+    }
+}
