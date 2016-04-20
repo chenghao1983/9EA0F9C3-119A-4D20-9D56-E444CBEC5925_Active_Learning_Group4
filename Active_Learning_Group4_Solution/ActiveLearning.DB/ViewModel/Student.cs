e4cc189114997f4f067a81f506b1fb9f599cad9e
gongsl <vinogong@gmail.com>
2016-04-20 20:52:50 +0800

    sl-update Student.cs

--- Active_Learning_Group4_Solution/ActiveLearning.DB/ViewModel/Student.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.DB/ViewModel/Student.cs
@@ -8,7 +8,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ActiveLearning.DB
 {
+    [MetadataType(typeof(UserMetadata))]
     public partial class Student
     {
+        private class UserMetadata
+        {
+            [Required(ErrorMessage = Common.Constants.Please_Enter + "Batch Number")]
+            [Display(Name = "Batch Number")]
+            public string BatchNo { get; set; }
+        }
     }
 }
