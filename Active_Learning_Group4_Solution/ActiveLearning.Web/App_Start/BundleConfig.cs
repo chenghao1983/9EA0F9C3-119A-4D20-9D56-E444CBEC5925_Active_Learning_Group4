577a7e608bbe347681c2b7b31da87ac620ec8ac6
sheng kiat <sheng kiat@DESKTOP-ESER2VA>
2016-04-21 14:14:37 +0800

    Update

--- Active_Learning_Group4_Solution/ActiveLearning.Web/App_Start/BundleConfig.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace ActiveLearning.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/flip.css"));
         }
     }
 }
