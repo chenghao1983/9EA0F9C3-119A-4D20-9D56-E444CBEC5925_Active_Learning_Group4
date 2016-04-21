d4a3510e6cccb544af512459486a780d14a2bb3b
sheng kiat <sheng kiat@DESKTOP-ESER2VA>
2016-04-21 14:06:26 +0800

    Update

---
+++ Active_Learning_Group4_Solution/ActiveLearning.Web/Controllers/QuizStatisticsController.cs
@@ -0,0 +1,21 @@
+﻿using ActiveLearning.Web.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActiveLearning.Web.Controllers
+{
+    public class QuizStatisticsController : Controller
+    {
+        // GET: QuizStatistics
+        public async Task<ActionResult> Index()
+        {
+            var statisticsService = new StatisticsService();
+
+            return View(await statisticsService.GenerateStatistics());
+        }
+    }
+}
