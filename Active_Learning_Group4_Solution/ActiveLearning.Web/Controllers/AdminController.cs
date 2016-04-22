40d578edf442f55c421c0eaecf1543e2b00d7f61
gongsl <vinogong@gmail.com>
2016-04-23 01:08:01 +0800

    shengliang- management of CRUD for Admin

--- Active_Learning_Group4_Solution/ActiveLearning.Web/Controllers/AdminController.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.Web/Controllers/AdminController.cs
@@ -23,29 +23,71 @@ namespace ActiveLearning.Web.Controllers
 
         #region Course
 
-        public ActionResult CreateCourse()
-        {
-            User user = new User();
-            user.Role = Constants.User_Role_Student_Code;
-            user.Username = "Joe";
 
-            LogUserIn(user);
-
-            ViewBag.Title = "Create Course";
-            return View();
-        }
-
-        [CustomAuthorize(Roles = Constants.User_Role_Admin_Code + "," + Constants.User_Role_Instructor_Code)]
-        [HttpPost]
-        public ActionResult CreateCourse(Course course)
+        // GET: ManageCourse
+        public ActionResult ManageCourse()
         {
             string message = string.Empty;
             using (var courseManager = new CourseManager())
             {
-                courseManager.AddCourse(course, out message);
+
+                List<Course> listCourse = (List<Course>)courseManager.GetAllCourses(out message);
+                if (listCourse == null || listCourse.Count == 0)
+                {
+                    ViewBag.Message = "The List is empty !";
+                }
+
+                return View(listCourse);
             }
-            ViewBag.Message = "Course Created !";
+
+        }
+
+        // GET: ManageCourse/CreateCourse
+        public ActionResult CreateCourse()
+        {
+
             return View();
+            //User user = new User();
+            //user.Role = Constants.User_Role_Student_Code;
+            //user.Username = "Joe";
+
+            //LogUserIn(user);
+
+            //ViewBag.Title = "Create Course";
+            //return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCourse(Course course)
+        {
+            try
+            {
+                string message = string.Empty;
+                using (var courseManager = new CourseManager())
+                {
+                    if (courseManager.AddCourse(course, out message) == null)
+                    {
+                        ViewBag.Message = message;
+                        return View();
+                    }
+                }
+                ViewBag.Message = "Course Created !";
+
+                return RedirectToAction("ManageCourse");
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(course);
+
+            //string message = string.Empty;
+            //using (var courseManager = new CourseManager())
+            //{
+            //    courseManager.AddCourse(course, out message);
+            //}
+            //ViewBag.Message = "Course Created !";
+            //return View();
         }
 
 
@@ -54,37 +96,147 @@ namespace ActiveLearning.Web.Controllers
 
         #region Instructor
 
-        public ActionResult CreateInstructor()
+        // GET: ManageInstructor
+        public ActionResult ManageInstructor()
         {
             string message = string.Empty;
-            if (!IsUserAuthenticated())
-            {
-                return RedirectToLogin();
-            }
             using (var userManager = new UserManager())
             {
 
-                userManager.GetAllActiveInstructor(out message);
+                List<Instructor> listInstructor = (List<Instructor>)userManager.GetAllActiveInstructor(out message);
+                if (listInstructor == null || listInstructor.Count == 0)
+                {
+                    ViewBag.Message = "The List is empty !";
+                }
+                return View(listInstructor);
             }
 
-            ViewBag.Title = "Create Instructor";
+        }
+
+        // GET: ManageInstructor/EditInstructor/6
+        public ActionResult EditInstructor(int id)
+        {
+            string message = string.Empty;
+            using (var getInstructor = new UserManager())
+            {
+                Instructor instructor = getInstructor.GetInstructorByInstructorSid(id, out message);
+                if (instructor == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(instructor);
+            };
+        }
+
+        // GET: ManageInstructor/CreateInstructor
+        public ActionResult CreateInstructor()
+        {
             return View();
+            //string message = string.Empty;
+            //if (!IsUserAuthenticated())
+            //{
+            //    return RedirectToLogin();
+            //}
+            //using (var userManager = new UserManager())
+            //{
+
+            //    userManager.GetAllActiveInstructor(out message);
+            //}
+
+            //ViewBag.Title = "Create Instructor";
+            //return View();
         }
 
 
-
+        // POST: ManageInstructor/CreateInstructor
         [HttpPost]
         public ActionResult CreateInstructor(Instructor instructor)
         {
-            // HttpContext.User.Identity
-            string message = string.Empty;
 
-            using (var userManager = new UserManager())
+            try
             {
-                userManager.AddInstructor(instructor, out message);
+                string message = string.Empty;
+                using (var userManager = new UserManager())
+                {
+                    if (userManager.AddInstructor(instructor, out message) == null)
+                    {
+                        ViewBag.Message = message;
+                        return View();
+                    }
+                }
+                ViewBag.Message = "Instructor Created !";
+
+                return RedirectToAction("ManageInstructor");
             }
-            ViewBag.Message = "Instructor Created !";
-            return View();
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(instructor);
+
+
+
+            // HttpContext.User.Identity
+            //string message = string.Empty;
+
+            //using (var userManager = new UserManager())
+            //{
+            //    userManager.AddInstructor(instructor, out message);
+            //}
+            //ViewBag.Message = "Instructor Created !";
+            //return View();
+        }
+
+        // GET: ManageInstructor/DeleteInstructor/6
+        public ActionResult DeleteInstructor(int id)
+        {
+            string message = string.Empty;
+            using (var deleteInstructor = new UserManager())
+            {
+                Instructor instructor = deleteInstructor.GetInstructorByInstructorSid(id, out message);
+                if (instructor == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(instructor);
+            };
+
+        }
+
+        // POST: ManageInstructor/DeleteInstructor/6
+        [HttpPost, ActionName("DeleteInstructor")]
+        public ActionResult DeleteIns(int id)
+        {
+            try
+            {
+                string message = string.Empty;
+                using (var deleteInstructor = new UserManager())
+                {
+                    //if (student == null)
+                    //{
+                    //    return HttpNotFound();
+                    //}
+                    Instructor instructor = deleteInstructor.GetInstructorByInstructorSid(id, out message);
+                    if (deleteInstructor.DeleteInstructor(instructor, out message))
+                    {
+                        return RedirectToAction("ManageInstructor");
+                    }
+                    return View(instructor);
+                };
+            }
+            catch (Exception e)
+            {
+                if (this.HttpContext.IsDebuggingEnabled)
+                {
+                    ModelState.AddModelError(string.Empty, e.ToString());
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Some technical error happened.");
+                }
+                return View();
+            }
+
         }
 
         #endregion
@@ -102,8 +254,28 @@ namespace ActiveLearning.Web.Controllers
             {
 
                 List<Student> listStudent = (List<Student>)userManager.GetAllStudent(out message);
+                if (listStudent == null || listStudent.Count == 0)
+                {
+                    ViewBag.Message = "The List is empty !";
+                }
+
                 return View(listStudent);
             }
+
+        }
+
+        public ActionResult StudentDetails(int id)
+        {
+            string message = string.Empty;
+            using (var getStudent = new UserManager())
+            {
+                Student student = getStudent.GetStudentByStudentSid(id, out message);
+                if (student == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(student);
+            };
 
         }
 
@@ -158,15 +330,15 @@ namespace ActiveLearning.Web.Controllers
         }
 
         // POST: ManageStudent/EditStudent/6
-        [HttpPost,ActionName("EditStudent")]
+        [HttpPost, ActionName("EditStudent")]
         public ActionResult update(Student student)
         {
             try
             {
                 string message = string.Empty;
-                using (var updateStudent=new UserManager())
+                using (var updateStudent = new UserManager())
                 {
-                    if(updateStudent.UpdateStudent(student,out message))
+                    if (updateStudent.UpdateStudent(student, out message))
                     {
                         return RedirectToAction("ManageStudent");
                     }
@@ -179,7 +351,8 @@ namespace ActiveLearning.Web.Controllers
                 if (this.HttpContext.IsDebuggingEnabled)
                 {
                     ModelState.AddModelError(string.Empty, e.ToString());
-                }else
+                }
+                else
                 {
                     ModelState.AddModelError(string.Empty, "Some technical error happened.");
                 }
@@ -206,7 +379,7 @@ namespace ActiveLearning.Web.Controllers
 
         // POST: ManageStudent/DeleteStudent/6
         [HttpPost, ActionName("DeleteStudent")]
-        public ActionResult Delete(Student student)
+        public ActionResult Delete(int id)
         {
             try
             {
@@ -217,6 +390,7 @@ namespace ActiveLearning.Web.Controllers
                     //{
                     //    return HttpNotFound();
                     //}
+                    Student student = deleteStudent.GetStudentByStudentSid(id, out message);
                     if (deleteStudent.DeleteStudent(student, out message))
                     {
                         return RedirectToAction("ManageStudent");
