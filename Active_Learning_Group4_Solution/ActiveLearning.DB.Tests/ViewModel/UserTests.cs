752ddda8b6f0b5606b72f25f18b4d2bb9831dfeb
sheng kiat <sheng kiat@DESKTOP-ESER2VA>
2016-04-24 00:34:04 +0800

    Added DB Test for viewmodel

---
+++ Active_Learning_Group4_Solution/ActiveLearning.DB.Tests/ViewModel/UserTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.DataAnnotations;
+
+namespace ActiveLearning.DB.Tests.ViewModel
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void Validate_Model_Given_Valid_Model_ExpectNoValidationErrors()
+        {
+            var model = new ActiveLearning.DB.User();
+
+            model.Username = "Andy Lau";
+            model.Password = "Tony Chiu";
+
+            TestModelHelper.ValidateObject(model);
+        }
+
+        [TestMethod]
+        public void Validate_Model_Given_Username_Is_Null_ExpectOneValidationError()
+        {
+            try
+            {
+                var model = new ActiveLearning.DB.User();
+
+                TestModelHelper.ValidateObject(model);
+            }
+            catch (ValidationException ae)
+            {
+                Assert.AreEqual(Common.Constants.Please_Enter + "User Name", ae.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Validate_Model_Given_Password_Is_Null_ExpectOneValidationError()
+        {
+            try
+            {
+                var model = new ActiveLearning.DB.User();
+
+                model.Username = "Andy Lau";
+
+                TestModelHelper.ValidateObject(model);
+            }
+            catch (ValidationException ae)
+            {
+                Assert.AreEqual(Common.Constants.Please_Enter + "Password", ae.Message);
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
--- Active_Learning_Group4_Solution/ActiveLearning.DB/ViewModel/User.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.DB/ViewModel/User.cs
@@ -11,11 +11,12 @@ namespace ActiveLearning.DB
     [MetadataType(typeof(UserMetadata))]
     public partial class User
     {
-        private class UserMetadata
+        public class UserMetadata
         {
             [Required(ErrorMessage = Common.Constants.Please_Enter + "User Name")]
             [Display(Name = "User Name")]
-            public string Username { get; set; }
+            public string Username { get;
+                set; }
 
             [Required(ErrorMessage = Common.Constants.Please_Enter + "Password")]
             [DataType(DataType.Password)]
