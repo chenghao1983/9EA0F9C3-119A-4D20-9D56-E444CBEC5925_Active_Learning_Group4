93d185b567d34611dfc0b0172f56dbae1dc652a3
chenghao1983 <joechenghao@gmail.com>
2016-04-25 22:31:04 +0800

    updated manager

--- Active_Learning_Group4_Solution/ActiveLearning.Business/Implementation/ContentManager.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.Business/Implementation/ContentManager.cs
@@ -1,15 +1,66 @@
-﻿using System;
+﻿using ActiveLearning.Business.Interface;
+using ActiveLearning.DB;
+using ActiveLearning.Repository;
+using ActiveLearning.Repository.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ActiveLearning.DB;
-using ActiveLearning.Business.Interface;
-using ActiveLearning.Business.Common;
 
 namespace ActiveLearning.Business.Implementation
 {
     public class ContentManager : BaseManager, IContentManager
     {
+        public Content AddContent(Content content, int courseSid, out string message)
+        {
+            message = string.Empty;
+            using (var unitOfWork = new UnitOfWork(new ActiveLearningContext()))
+            {
+                // Same course cannot have same file name
+                //var content = unitOfWork.Contents.GetAll().Where(c => c.CourseSid == content.CourseSid && c.DeleteDT == null && c.OriginalFileName == content.OriginalFileName).FirstOrDefault();
+
+
+                if (content == null)
+                {
+                    unitOfWork.Contents.Add(content);
+
+                    unitOfWork.Complete();
+                    return content;
+                }
+                else
+                {
+                    throw new Exception("Same filename exits.");
+                }
+            }
+        }
+
+        public bool DeleteContent(Content content, out string message)
+        {
+            message = string.Empty;
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Content> GetContentsByCourseSid(int courseID, out string message)
+        {
+            message = string.Empty;
+            using (var unitOfWork = new UnitOfWork(new ActiveLearningContext()))
+            {
+                return unitOfWork.Contents.GetAll().Where(c => c.CourseSid == courseID && c.DeleteDT == null);
+            }
+        }
+
+
+
+        public string GetGUIDFile(string originalFilename, int courseID, out string message)
+        {
+            message = string.Empty;
+            using (var unitOfWork = new UnitOfWork(new ActiveLearningContext()))
+            {
+                var file = unitOfWork.Contents.GetAll().Where(c => c.CourseSid == courseID && c.DeleteDT == null && c.OriginalFileName == originalFilename).FirstOrDefault();
+
+                return file.FileName;
+            }
+        }
     }
 }
--- Active_Learning_Group4_Solution/ActiveLearning.Business/Interface/IContentManager.cs
+++ Active_Learning_Group4_Solution/ActiveLearning.Business/Interface/IContentManager.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using ActiveLearning.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ActiveLearning.DB;
 
 namespace ActiveLearning.Business.Interface
 {
     public interface IContentManager
     {
-        }
+        Content AddContent(Content content, int courseSid, out string message);
+
+        bool DeleteContent(Content conten, out string message);
+
+        IEnumerable<Content> GetContentsByCourseSid(int courseSid, out string message);
+
+        string GetGUIDFile(string originalFilename, int courseID, out string message);
+
+    }
 }
